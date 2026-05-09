7baa7c791ab373933fcad5f662f6e77fb850e267
joaoelias10 <joaoe.martins7@gmail.com>
2026-05-09 01:31:19 -0300

    clean struct, hrefs, correção

---
+++ js/env.js
@@ -0,0 +1,4 @@
+window.ENV = {
+  SUPABASE_URL: 'https://rfptgcsmxgvqxbwzthic.supabase.co',
+  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmcHRnY3NteGd2cXhid3p0aGljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwMDY2NTUsImV4cCI6MjA5MzU4MjY1NX0.6p99HIP_BuFuQMKV-4hf4orc2HCBTLQHNfAVUyKnUh4'
+};
